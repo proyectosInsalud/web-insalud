e8b54e3a751bccdea3d065f8c71edc228b832530
M4rckR <marcromerogar4@gmail.com>
2025-08-23 10:04:23 -0500

    Update redireccion paginas en pauta v2

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,7 @@ const nextConfig: NextConfig = {
       { source: "/nosotros", destination: "/", permanent: true },
       { source: "/sedes", destination: "/contacto", permanent: true },
       { source: "/jesusmaria-chequeo-prostatico-2/", destination: "/contacto", permanent: true },
+      { source: "/jesusmaria-chequeo-prostatico-2", destination: "/contacto", permanent: true },
 
       // Media heredada de WordPress (evita imágenes rotas de enlaces antiguos)
       {
