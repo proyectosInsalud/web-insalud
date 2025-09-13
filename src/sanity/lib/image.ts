40742d296bf0950fce6de5df88a15a605c5ef236
¨M4rckR¨ <marcromerogar2@gmail.com>
2025-09-13 09:54:30 -0500

    salvado por daniela

--- src/sanity/lib/image.ts
+++
@@ -1,6 +0,0 @@
-import createImageUrlBuilder from '@sanity/image-url';
-import type {SanityImageSource} from '@sanity/image-url/lib/types/types';
-import {dataset, projectId} from '../env';
-
-const builder = createImageUrlBuilder({projectId, dataset});
-export const urlFor = (src: SanityImageSource) => builder.image(src);
