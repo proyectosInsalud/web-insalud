ce9f2fab2f85d3d7167fdf86fef4ac576bd2bb69
M4rckR <marcromerogar4@gmail.com>
2025-09-18 17:42:32 -0500

    Sitemaps listo

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,32 +8,74 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: `${siteUrl}/`,
       lastModified: now,
-      changeFrequency: "weekly",
-      priority: 1,
     },
     {
       url: `${siteUrl}/diagnosticos`,
       lastModified: now,
-      changeFrequency: "monthly",
-      priority: 0.8,
     },
     {
       url: `${siteUrl}/servicios`,
       lastModified: now,
-      changeFrequency: "monthly",
-      priority: 0.8,
     },
     {
       url: `${siteUrl}/contacto`,
       lastModified: now,
-      changeFrequency: "monthly",
-      priority: 0.8,
     },
     {
       url: `${siteUrl}/servicios/chequeo-prostatico`,
       lastModified: now,
-      changeFrequency: "monthly",
-      priority: 0.6,
+    },
+    {
+      url: `${siteUrl}/blog`,
+      lastModified: now,
+    },
+    {
+      url: `${siteUrl}/blog/problemas-urologicos-en-jovenes-propensos-a-enfermedades-de-transmision-sexual`,
+      lastModified: now,
+    },
+    {
+      url: `${siteUrl}/blog/insalud-empresa-peruana-del-ano-2025-en-salud-especializada`,
+      lastModified: now,
+    },
+    {
+      url: `${siteUrl}/blog/in-aesthetics-descubre-tu-belleza-y-libera-tu-mejor-version`,
+      lastModified: now,
+    },
+    {
+      url: `${siteUrl}/blog/hombres-jovenes-en-riesgo-por-vph`,
+      lastModified: now,
+    },
+    {
+      url: `${siteUrl}/blog/dieta-clave-para-mejorar-la-salud-de-la-prostata`,
+      lastModified: now,
+    },
+    {
+      url: `${siteUrl}/blog/insalud-busca-conquistar-latinoamerica-al-2027`,
+      lastModified: now,
+    },
+    {
+      url: `${siteUrl}/blog/peruana-insalud-llegara-a-mexico-y-tres-paises-mas-en-2025`,
+      lastModified: now,
+    },
+    {
+      url: `${siteUrl}/blog/rompiendo-el-silencio-la-salud-sexual-como-pilar-del-bienestar`,
+      lastModified: now,
+    },
+    {
+      url: `${siteUrl}/blog/problemas-de-salud-sexual-en-el-peru`,
+      lastModified: now,
+    },
+    {
+      url: `${siteUrl}/blog/revolucionario-tecnica-para-tratar-disfuncion-erectil`,
+      lastModified: now,
+    },
+    {
+      url: `${siteUrl}/blog/castracion-quimica-de-que-se-trata-la-propuesta-del-presidente-castillo`,
+      lastModified: now,
+    },
+    {
+      url: `${siteUrl}/blog/cuidados-urologicos-en-mujeres`,
+      lastModified: now,
     },
   ];
 }
