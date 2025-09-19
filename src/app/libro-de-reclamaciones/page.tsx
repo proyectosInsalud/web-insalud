155b3f84a2eee5b810ca17b0382182b57e4062ce
M4rckR <marcromerogar4@gmail.com>
2025-09-19 18:05:31 -0500

    Libro de reclamaciones agregado v2

--- src/app/libro-de-reclamaciones/page.tsx
+++ src/app/libro-de-reclamaciones/page.tsx
@@ -14,6 +14,7 @@ export default function LibroDeReclamaciones() {
         <div className="hidden md:block">
             <CintilloBarra />
         </div>
+        {/* Coment */}
             <NavBarIntern />
             <FormContactLibro />
         </>
